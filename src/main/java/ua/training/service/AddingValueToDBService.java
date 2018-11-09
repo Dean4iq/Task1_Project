8809d018921d18be1d095b3314b3d34372abab23
Dean4iq <denis.ua22@gmail.com>
2018-11-09 14:12:07 +0200

    Resolved localization issues & Added service classes

---
+++ src/main/java/ua/training/service/AddingValueToDBService.java
@@ -0,0 +1,40 @@
+package ua.training.service;
+
+import ua.training.model.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+class AddingValueToDBService {
+    private AddingValueToDBService(){}
+
+    static void addValueToList(HttpServletRequest req, Controller controller) {
+        final String name = req.getParameter("name");
+        final String lastName = req.getParameter("lastname");
+        final String nickname = req.getParameter("nickname");
+        final String phone = req.getParameter("phone");
+        final String id = req.getParameter("id");
+
+        if (!name.equals("") || !lastName.equals("") || !nickname.equals("") ||
+                !phone.equals("") || !id.equals("")) {
+            final FullContactData fullContactData = new FullContactData();
+
+            if (!name.equals("")) {
+                fullContactData.setName(new NameContact(name));
+            }
+            if (!lastName.equals("")) {
+                fullContactData.setLastName(new LastNameContact(lastName));
+            }
+            if (!nickname.equals("")) {
+                fullContactData.setNickname(new NicknameContact(nickname));
+            }
+            if (!phone.equals("")) {
+                fullContactData.setPhone(new PhoneContact(phone));
+            }
+            if (!id.equals("")) {
+                fullContactData.setId(new IdContact(id));
+            }
+
+            controller.addContactToList(fullContactData);
+        }
+    }
+}
---
+++ src/main/java/ua/training/service/UnitingService.java
@@ -0,0 +1,9 @@
+package ua.training.service;
+
+class UnitingService {
+    private UnitingService(){}
+
+    static void uniteRows(){
+        //TODO
+    }
+}
