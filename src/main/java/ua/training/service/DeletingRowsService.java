802116165e74c0c211a40f1cef6e64b6ca618574
Dean4iq <denis.ua22@gmail.com>
2018-11-09 15:43:59 +0200

    Deleting service added

---
+++ src/main/java/ua/training/service/DeletingRowsService.java
@@ -0,0 +1,21 @@
+package ua.training.service;
+
+import ua.training.util.DBQueries;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+class DeletingRowsService {
+    private DeletingRowsService(){}
+
+    static void deleteRows(HttpServletRequest request, Controller controller){
+        //TODO
+        String[] selectedRowsId = request.getParameterValues("selectedRows");
+
+        StringBuilder stringBuilder = new StringBuilder().append(DBQueries.DELETE_BY_ID_QUERY);
+        Arrays.stream(selectedRowsId).forEach(elem -> stringBuilder.append(elem).append(","));
+        stringBuilder.deleteCharAt(stringBuilder.length()-1).append(")");
+        controller.getDbConnector().executeQuery(stringBuilder.toString());
+        controller.getDataFromDatabase();
+    }
+}
